6cd40dfedb2f2335d6bad2ccf0b4316a67d8cdf3
Ammogarden <xujiangd@usc.edu>
2022-11-20 23:37:33 -0800

    Jiang (#20)
    * small fix
    
    * add greater or equal, less or equal, and some optimization & code style fix

--- src/query.py
+++ src/query.py
@@ -1,7 +1,8 @@
+from datetime import datetime as dt
+import csv
 from MongoFS import MongoFS
 import ordertools as SqlFS
 import firebase_commands as FbFS
-import csv
 
 
 # configs
@@ -11,7 +12,7 @@ database=""
 mongoClient = MongoFS(mongo_conn_str)
 
 
-def getPartition(file,db):
+def getPartitionCount(file, db):
     # get file's number of partitions from database db
     # return the number
     try:
@@ -26,47 +27,56 @@ def getPartition(file,db):
     except Exception as e:
         print("Wrong Database")
 
-def getHeader(file, db):
+def getHeader(firstPartition):
     '''
-        getHeader reads the first partition of {file} in {db}, then use the first line
-        to get attribute information. Returns a map<string, int> that maps attribute name to index in row (0-indexed)
+        getHeader reads the first line of the first partition to get attribute information.
+        Returns a map<string, int> that maps attribute name to index in row (0-indexed)
         e.g. an csv table with schema ['id', 'name'], and value ['33', 'John Smith']. This functions will return a map:
         {'id': 0, 'name': 1}
     '''
-    res = []
-    try:
-        if db == 'mongo':
-            res = mongoClient.readPartition(file, 1)
-        elif db == 'mysql':
-            res = SqlFS.readPartition(file, 1)
-        elif db == 'firebase':
-            res = FbFS.readPartition(file, 1)
-    except:
-        print("ERROR: getHeader() read first partition of " + file + "  from " + db)
-
     attributeNameToIndex = {}
-    reader = csv.reader([res[0]], delimiter=',')
+    reader = csv.reader([firstPartition[0]], delimiter=',')
     firstLine = reader.__next__()
     for i in range(len(firstLine)):
         attributeNameToIndex[firstLine[i]] = i
     return attributeNameToIndex
 
 
-def mapPartition(p, header, args, cal):
+def getNumOrDate(inputStr):
+    '''
+        getNumOrDate parse the input string and return one of following value:
+        if inputStr does not has "/", return as float
+        if inputStr has "/", treat as date in format "MM/DD/YYYY" and return datetime object
+        if either case raises exception, simply return itself
+    '''
+    try:
+        if inputStr.find("/") < 0:
+            return float(inputStr)
+
+        return dt.strptime(inputStr, "%m/%d/%Y")
+    except:
+        return inputStr
+
+
+def mapPartition(p, header, argsEq, argsGte, argsLte, cal):
     '''
         mapPartition() works as the mapper function.
         It processes the content of a partition and generate output
 
         :param p: partition content, list of strings with each string representing a row in the document
         :param header: csv header info, map<string, int>. It is used to get the index for an attribute in a row. e.g. header.get('second_attribute') == 1
-        :param args: a map with key being the attribute name, and val being the target value. Only those tuples that has exact same attribute value will be keeped
-        :param cal: a string indicating extra processing method we want to apply, MIN/MAX/COUNT  FIXME: how?
+        :param argsEq: a map with key being the attribute name, and val being the target value. Only those tuples that has exact same attribute value will be keeped
+        :param argsGte: a map with key being the attribute name, and val being the target value. Only those tuples that has greater or equal attribute value will be keeped
+        :param argsLte: a map with key being the attribute name, and val being the target value. Only those tuples that has smaller or equal attribute value will be keeped
+        :param cal: a string indicating extra processing method we want to apply, currently only support 'COUNT'
     '''
-    ans = []
+    ans = 0 if cal == 'COUNT' else []
+
     reader = csv.reader(p, delimiter=',') # assume all files need to be processed are csv files
     for tuple in reader:
         isMatch = True
-        for attribute, targetValue in args.items():
+
+        for attribute, targetValue in argsEq.items():
             if attribute not in header:
                 print("WARN: invalid attribute " + attribute)
                 continue
@@ -74,14 +84,38 @@ def mapPartition(p, header, args, cal):
             index = header.get(attribute)
             if index >= len(tuple) or tuple[index] != targetValue:
                 isMatch = False
+                break
+        if not isMatch:
+            continue
+
+        for attribute, lteValue in argsLte.items():
+            if attribute not in header:
+                print("WARN: invalid attribute " + attribute)
                 continue
+
+            index = header.get(attribute)
+            if index >= len(tuple) or getNumOrDate(tuple[index]) > getNumOrDate(lteValue):
+                isMatch = False
+                break
+        if not isMatch:
+            continue
+
+        for attribute, gteValue in argsGte.items():
+            if attribute not in header:
+                print("WARN: invalid attribute " + attribute)
+                continue
+
+            index = header.get(attribute)
+            if index >= len(tuple) or getNumOrDate(tuple[index]) < getNumOrDate(gteValue):
+                isMatch = False
+                break
+
         if isMatch:
-            ans.append(tuple)
+            if cal == 'COUNT':
+                ans += 1
+            else:
+                ans.append(tuple)
 
-    if cal == 'COUNT':
-        return len(ans)
-
-    # if cal is not defined, just return all matching tuples
     return ans
 
 def combine(mapPartitionResults, cal):
@@ -106,39 +140,51 @@ def combine(mapPartitionResults, cal):
         ans += mapResult
     return ans
 
-def manage(args, table, db, cal=False):
-    # args: {} all fields and their values
-    # table: String location of the table eg. /crime/Crime_data.csv
-    # db: String
+
+def manage(table, database, argsEq, argsGte, argsLte, cal=False):
+    # table: String represeting location of the table in EDFS eg. /crime/Crime_data.csv
+    # database: String representing database, one of ["mongo", "firebase", "mysql"]
+    # argsEq, argsGte, argsLte: a map with key being the attribute name, and val being the target value. Used for "equal", "greater or equal" and "less or equal" opearation
     # cal: String eg. min, max, count
-    database = db
     # get the number of partitions
-    partitions=getPartition(table,database)
+    partitionCount = getPartitionCount(table, database)
+    
     # get header of the csv file
-    header=getHeader(table,db)
-    results=[]
-    for partition in range(partitions):
-        if db == 'mongo':
-            p = mongoClient.readPartition(table,partition+1)
-        elif db == 'mysql':
-            p = SqlFS.readPartition(table,partition+1)
+    header = {}
+    results = []
+    for partitionNum in range(1, partitionCount):
+        if database == 'mongo':
+            p = mongoClient.readPartition(table, partitionNum)
+        elif database == 'mysql':
+            p = SqlFS.readPartition(table, partitionNum)
         else:
-            p = FbFS.readPartition(table,partition+1)
-        result=mapPartition(p,header,args,cal)
+            p = FbFS.readPartition(table, partitionNum)
+
+        # if this is the first partition, use its first line as header, and only use remaing rows as data
+        if partitionNum == 1:
+            header = getHeader(p)
+            p = p[1:]
+
+        result = mapPartition(p, header, argsEq, argsGte, argsLte, cal)
         results.append(result)
-    res=combine(results,cal)
+
+    res = combine(results, cal)
     return res
 
 
 def main():
-    args = {}
-    args['Sex Code'] = 'M'
-    args['Arrest Type Code'] = 'I'
-    res = manage(args, '/crime/arrest.csv', 'mongo', 'COUNT')
-    
+    argsEq = {}
+    argsGte = {}
+    argsLte = {}
+
+    argsEq['Sex Code'] = 'M'
+    argsEq['Arrest Type Code'] = 'I'
+    argsGte['Arrest Date'] = '01/01/2022'
+    argsLte['Arrest Date'] = '01/31/2022'
+
+    res = manage('/crime/arrest.csv', 'mongo', argsEq, argsGte, argsLte)
+
     print(res)
-    #for row in res:
-        #print(row)
 
     mongoClient.close()
     return 0
