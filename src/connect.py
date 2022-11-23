7548014cd949cf034702f68ea76903b782f592bb
Tiangliang <578667035@qq.com>
2022-11-22 23:18:33 -0800

    1. add comments 2. fix bug about rm

--- src/connect.py
+++ src/connect.py
@@ -2,10 +2,9 @@ import pandas as pd
 
 import ordertools as ot
 file = "../data/arrest_30.csv"
-# ot.mkdir("/user/a")
+ot.rm("//")
 # ot.mkdir("/user/b")
 # ot.put("file", "/user/b", 5)
 # ot.getPartitionLocations("/user/b/arrest_30.csv")
 # ot.readPartition("/user/b/arrest_30.csv", 4)
-df = pd.read_csv("../data/arrest_30.csv")
-print(df.info)
+# df = pd.read_csv("../data/arrest_30.csv")
--- src/ordertools.py
+++ src/ordertools.py
@@ -6,16 +6,33 @@ import collections
 pymysql.install_as_MySQLdb()
 
 address = "mysql+mysqldb://admin:dsci-551@database-2.cxay4obryyr9.us-west-1.rds.amazonaws.com:3306/dsci551project"
+# address = "mysql+mysqldb://root:123456@localhost/dsci551project"
 my_conn = sqlalchemy.create_engine(address)
 
 
 def __findpath(order):
     # order Example: /john/a
-    if order == "//" or "/":
+    """
+    function: recieve a path, return the relative information about the target file
+    return:
+        sign: Bool value, represent whether the function find the path successful
+        c_id: the id of current file
+        orderlist[i:]: the function also return the order name slice behind current file name
+    example:
+        the file system exist: /john/b
+        run function __findpath('/john/b')--> it will return True, the id of /john/b, []
+        run function __findpath('/john/a')--> it will return False,the id of /john,['b']
+        run function __findpath('//') --> it will return True, the id of root,['john','b']
+        the file system is empty(it is not the correct status):
+        run function __findpath('/john/b')--> False ,"", ['', 'user', 'b']
+        the file only contain root file:
+        run function __findpath('/john/b')--> False ,id of root, ['user', 'b']
+    """
+    if order == "//" or order == "/":
         orderlist = [""]
     else:
         orderlist = order.split('/')  # ["","john","a"]
-
+    print("orderlist:--> ", orderlist)
     c_id = ""  # c_id is current file id
 
     i = 0
@@ -28,16 +45,24 @@ def __findpath(order):
             t_list = pd.read_sql(f"SELECT t.id FROM (SELECT * FROM `meta data` WHERE filename = '{filename}') AS t, "
                                  f"`directory` AS d WHERE d.`children_id` = t.id AND `parent_id` = {c_id}", my_conn)
         if t_list.empty:
+            print(False, c_id, orderlist[i:])
             return False, c_id, orderlist[i:]
         else:
             c_id = t_list['id'][0]
+    print(True, c_id, orderlist[i:])
     return True, c_id, orderlist[i:]
 
 
 def __create_file_path(orderlist, start_id):
-
+    """
+        function: recieve a orderlist and start_id, edit(create) the table of directory and meta data
+        process:
+            if pid is "", create the root(it is not the correct case)
+            else edit the directory and meta data tables step by step
+        tips:
+            mtime is an useless variable now
+    """
     mtime = 0  # mtime is stable now, it should be changed in the future version
-
     p_id = start_id
     for name in orderlist:
         # create from empty table
@@ -58,15 +83,23 @@ def __create_file_path(orderlist, start_id):
 
 
 def __delete_file_path(start_id):
+    """
+            function: recieve a start_id, delete all the id and file under the start id in the tree structure(include start id)
+            process:
+                if pid is "", create the root(it is not the correct case)
+                else edit the directory and meta data tables by bfs algorithm
+            tips:
+    """
     # use bfs to delete the file
     queue = collections.deque()
     queue.append(start_id)
     id_list = []
     while queue:
         t = queue.popleft()
-        id_list.append(t)
         signlist = pd.read_sql(f"SELECT filename,isFile FROM `meta data` WHERE id = {t}", my_conn)
         sign = signlist["isFile"][0]
+        if signlist["filename"][0] != "":
+            id_list.append(t)
         if sign:
             my_conn.execute(f"DROP TABLE `fileid_{t}`")
         children = list(pd.read_sql(f"SELECT children_id AS id FROM DIRECTORY WHERE parent_id = {t}", my_conn)["id"])
@@ -83,6 +116,12 @@ def __delete_file_path(start_id):
 
 
 def __create_file(c_id, dataframe, p):
+    """
+        function: recieve a c_id, pandas dataframe and partitions number
+        process:
+                create a file table with c_id, in the system, we use hash partitions
+        tips:
+    """
     name = f"fileid_{c_id}"
     dataframe.to_sql(name=name, con=my_conn)
     my_conn.execute(f"ALTER TABLE `{name}` PARTITION BY HASH(`index`) PARTITIONS {p};")
@@ -97,8 +136,8 @@ def mkdir(order):
         __create_file_path(iteration, c_id)
         return True
     print("the path error or have exist!")
+    print(tsign, c_id, iteration)
     return False
-
 
 
 def rm(order):
@@ -107,11 +146,9 @@ def rm(order):
     tsign, c_id, iteration = __findpath(order)
     if not tsign:
         print("the path invalid")
-
         return False
     __delete_file_path(c_id)
     return True
-
 
 
 def put(filename, order, k):
@@ -133,7 +170,6 @@ def put(filename, order, k):
         return False
 
 
-
 def getPartitionLocations(order):
     tsign, c_id, iteration = __findpath(order)
     if not tsign:
@@ -146,7 +182,6 @@ def getPartitionLocations(order):
     return res.values.tolist()
 
 
-
 def readPartition(order, partition):
     tsign, c_id, iteration = __findpath(order)
     if not tsign:
@@ -157,6 +192,7 @@ def readPartition(order, partition):
     print(res)
 
     return res.values.tolist()
+
 
 # # order example: /dsci551project
 # return all tables in dsci551project
@@ -184,8 +220,6 @@ def ls(order, my_conn):
         return res
     # 1. if root directory is not the proeject database
     # 2. if input table doesn't exist in dsci551project
-
-
 
 # example:  '/dsci551project/LAPD_call/Area_Occ'
 # return all data entries in Area_Occ column
@@ -223,5 +257,3 @@ def cat(order, my_conn):
         for i in result:
             res.append(i)
         return res
-
-
