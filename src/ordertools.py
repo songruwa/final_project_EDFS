87af5717c9773a17c0075597043be3c84359936f
Songru Wang <michaelsong6918@gmail.com>
2022-11-23 12:03:33 -0800

    add ls&cat

--- src/ordertools.py
+++ src/ordertools.py
@@ -2,6 +2,7 @@ import pymysql
 import sqlalchemy
 import pandas as pd
 import collections
+import itertools
 
 pymysql.install_as_MySQLdb()
 
@@ -196,64 +197,46 @@ def readPartition(order, partition):
 
 # # order example: /dsci551project
 # return all tables in dsci551project
-def ls(order, my_conn):
-    order = "" if order == "/" else order
+def ls(order):
+    # eg: ls /dsci551project
+    # it will return all child file name of dsci551project
+    tsign, c_id, iteration = __findpath(order)
+    if not tsign:
+        print("the path invalid")
+        return False
+    # now has folder id; find folder id in direcdtory, then find all file whose parent_id equal to folder id
+    # first find child_id in `meta data`
+    query = 'SELECT children_id FROM directory WHERE parent_id = {id}'.format(id=c_id)
+    df = pd.read_sql(query,my_conn)
 
-    # orderlist result: ['','dsci551project']
-    orderlist = order.split('/')
+    # second, find file name based on child_id in `meta data`
+    res = []
+    for i in df['children_id'].tolist():
+        n_query = 'SELECT filename FROM `meta data` WHERE id = {id}'.format(id=i)
+        daf = pd.read_sql(n_query,my_conn)
+        res.append(daf['filename'].tolist())
+    res = list(itertools.chain.from_iterable(res))
 
-    if orderlist[1] != 'dsci551project' or orderlist[2] not in my_conn.table_names():
-        return 'Input path does not exist'
-
-    # print all tables in database- dsci551project
-    if len(orderlist) == 2 and orderlist[1] == 'dsci551project':
-        return my_conn.table_names()
-    # print column names in a target table
-    # eg: '/dsci551project/LAPD_call'
-    elif len(orderlist) == 3 and orderlist[1] == 'dsci551project':
-        # show table's column names
-        insp = inspect(my_conn)
-        columns_table = insp.get_columns(orderlist[2])
-        res = []
-        for c in columns_table[2:]:
-            res.append(c['name'])
-        return res
-    # 1. if root directory is not the proeject database
-    # 2. if input table doesn't exist in dsci551project
+    return res
 
 # example:  '/dsci551project/LAPD_call/Area_Occ'
 # return all data entries in Area_Occ column
 # other instance: '/dsci551project/LAPD_call'
 # return all table data entries in LAPD_call
-def cat(order, my_conn):
-    order = "" if order == "/" else order
-    orderlist = order.split('/')
-
-    # 1. if root directory is not the proeject database
-    # 2. if input table doesn't exist in dsci551project
-    if orderlist[1] != 'dsci551project' or orderlist[2] not in my_conn.table_names():
-        return 'Input path does not exist'
-    insp = inspect(my_conn)
-    columns_table = insp.get_columns(orderlist[2])
-    res = []
-    for c in columns_table[2:]:
-        res.append(c['name'])
-    if orderlist[3] not in res:
-        return 'Input path does not exist'
-
-    if len(orderlist) == 2 and orderlist[1] == 'dsci551project':
-        return 'This is a foler, not a file, cannot show contents'
-    # print column entries in a target column
-    # eg: '/dsci551project/LAPD_call'
-    elif len(orderlist) == 3 and orderlist[1] == 'dsci551project':
-        user_table = pd.read_sql_table(table_name=orderlist[2], con=my_conn)
-        return user_table
-    # print all entries in a target column
-    # eg: '/dsci551project/LAPD_call/Area_Occ'
-    elif len(orderlist) == 4 and orderlist[1] == 'dsci551project':
-        query = f'SELECT {orderlist[3]} FROM {orderlist[2]} ;'
-        result = my_conn.execute(query)
-        res = []
-        for i in result:
-            res.append(i)
-        return res
+def cat(order):
+    tsign, c_id, iteration = __findpath(order)
+    if not tsign:
+        print("the path invalid")
+        return False
+    # first, even though it's a valid path, determine whether it's a file
+    # if not a file, return 'the path only have directory'
+    nn_query = 'SELECT isFile FROM `meta data` WHERE id = {id}'.format(id=c_id)
+    df = pd.read_sql(nn_query, my_conn)
+    if 0 in df['isFile'].tolist():
+        return 'Not valid; the path only have directory'
+    elif 1 in df['isFile'].tolist():
+        # why table name in this way?
+        # bc when in database imported table's name is in this style: 'fileid_fileID'
+        nn_query = 'SELECT * FROM {table}'.format(table='fileid_'+str(c_id))
+        df = pd.read_sql(nn_query, my_conn)
+        return df
